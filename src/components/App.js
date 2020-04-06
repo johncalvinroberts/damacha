6853a51a4a20d0680da8c49a297dc8ec8c756da9
John Roberts <john.calvin.roberts@gmail.com>
2020-04-07 01:22:09 +0800

    edit theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'theme-ui';
+import { EditorProvider } from '@theme-ui/editor';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import theme from './theme';
 import Container from './Container';
@@ -7,22 +8,26 @@ import Beats from './Beats';
 import Favicon from './Favicon';
 import Tracks from './Tracks';
 import Track from './Track';
+import Editor from './Editor';
 import NotFound from './NotFound';
 
 export default () => {
   return (
     <Router>
       <ThemeProvider theme={theme}>
-        <Favicon />
-        <Beats>
-          <Container>
-            <Routes>
-              <Route path="/" element={<Tracks />} />
-              <Route path="/:slug" element={<Track />} />
-              <Route path="/not-found" element={<NotFound />} />
-            </Routes>
-          </Container>
-        </Beats>
+        <EditorProvider>
+          <Favicon />
+          <Beats>
+            <Container>
+              <Routes>
+                <Route path="/" element={<Tracks />} />
+                <Route path="/:slug" element={<Track />} />
+                <Route path="/edit-themes" element={<Editor />} />
+                <Route path="/not-found" element={<NotFound />} />
+              </Routes>
+            </Container>
+          </Beats>
+        </EditorProvider>
       </ThemeProvider>
     </Router>
   );
---
+++ src/components/Editor.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Theme } from '@theme-ui/editor';
+
+export default () => (
+  <div>
+    <Theme.Fonts />
+    <Theme.FontSizes />
+    <Theme.FontWeights />
+    <Theme.LineHeights />
+    <Theme.Colors />
+    <Theme.Space />
+  </div>
+);

07456ca81e88786d04dbb447ad86b67310af3051
John Roberts <john.calvin.roberts@gmail.com>
2020-04-06 20:24:13 +0800

    add track

--- src/index.js
+++ src/index.js
@@ -4,17 +4,7 @@ import App from './components/App';
 
 const rootElement = document.getElementById('root');
 
-if (process.env.NODE_ENV === 'production') {
-  window.fathom = { q: [] };
-  window.snapSaveState = () => ({
-    fathom: {
-      q: [
-        ['set', 'siteId', 'IKNUK'],
-        ['set', 'spa', 'pushstate'],
-      ],
-    },
-  });
-}
+window.env = process.env.NODE_ENV;
 
 if (rootElement.hasChildNodes()) {
   ReactDOM.hydrate(<App />, rootElement);
